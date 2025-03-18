d1fe56458968147aa43b8bc8a0c23178096a96a6
LokeshGangineni <lokeshgangineni1329@gmail.com>
2025-03-18 19:34:50 +0530

    new commit done

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -7,7 +7,6 @@ export default function Textform(props) {
     
     const handleUpClick=(event)=>
     {
-      // console.log("clicked uppercase"+text);
       console.log("clicked on to uppercase");
       var newStr=text.toUpperCase();
       setText(newStr);
@@ -15,7 +14,6 @@ export default function Textform(props) {
     }
     const handleLoClick=(event)=>
     {
-      // console.log("clicked uppercase"+text);
       console.log("clicked on to uppercase");
       var newStr=text.toLowerCase();
       setText(newStr);
@@ -34,7 +32,6 @@ export default function Textform(props) {
       <div className="container">
         <h1 >{props.heading}</h1>
         <div  className="mb-3">
-            {/* <label htmlFor="myBox">Example TextArea</label> */}
             <br />
             <textarea style={{width:'80em'}} name="" id="myBox" rows="9" value={text} onChange={handleOnChange} ></textarea>
         </div>

910dbc7ae9839bb7ddb0d14534cbfc6bd6e9ef10
LokeshGangineni <lokeshgangineni1329@gmail.com>
2025-05-14 15:28:35 +0530

    second commit

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -41,7 +41,7 @@ export default function Textform(props) {
     {
       let text=document.getElementById("myBox");
       text.select();
-      navigator.clipboard.writeText(text.value);
+      navigator.clipboard.writeText(text.value );
     } 
   
   return (
